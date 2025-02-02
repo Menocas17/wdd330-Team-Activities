c4247440400b4687d2e5d4e1864a04f3a0071187
Menocas17 <rodolfomenocal7@gmail.com>
2025-02-01 18:14:21 -0600

    adding the filter to only show the 4 tents needed

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -39,3 +39,20 @@ export function getParams (param) {
 
   return product;
 }
+
+// function to use a template when needed 
+
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  const htmlStrings = list.map(templateFn);
+
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
